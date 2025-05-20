fc04b770235bb097a240f80768f000b0176845ad
wabwirevalentine <levaksmaita@gmail.com>
2025-05-20 14:38:28 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log("Good!");
+  };
+  const handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <div>
+      <button onBlur={handleBlur} onFocus={handleFocus}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
